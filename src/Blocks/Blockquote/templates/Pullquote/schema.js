509ebca829393a3455737ab4d2b295de0a373053
Miu Razvan <miu.razvan28@gmail.com>
2022-03-17 10:50:54 +0200

    fix conflict

--- src/Blocks/Blockquote/templates/Pullquote/schema.js
+++ src/Blocks/Blockquote/templates/Pullquote/schema.js
@@ -9,12 +9,7 @@ export default {
   ],
   properties: {
     position: {
-<<<<<<< Updated upstream
       title: 'Alignment',
-=======
-      title: 'Quote position',
-      // description: 'Takes effect only in view mode',
->>>>>>> Stashed changes
       widget: 'align',
     },
   },
