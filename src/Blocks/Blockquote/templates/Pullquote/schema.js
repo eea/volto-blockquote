348b23b08f16a12b01cfea6352a2a59ba8af8384
Miu Razvan <miu.razvan28@gmail.com>
2022-03-17 10:49:53 +0200

    Remove uneeded description

--- src/Blocks/Blockquote/templates/Pullquote/schema.js
+++ src/Blocks/Blockquote/templates/Pullquote/schema.js
@@ -9,7 +9,12 @@ export default {
   ],
   properties: {
     position: {
+<<<<<<< Updated upstream
       title: 'Alignment',
+=======
+      title: 'Quote position',
+      // description: 'Takes effect only in view mode',
+>>>>>>> Stashed changes
       widget: 'align',
     },
   },
